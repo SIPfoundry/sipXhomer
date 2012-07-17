aedd6cb848b455eb65b0fbe7c31d857ad42e8a5d
Douglas Hubler <dhubler@ezuce.com>
2012-07-17 11:01:16 -0400

    fix cfe error editing configuration.php

--- webhomer-auth/auth.php
+++ webhomer-auth/auth.php
@@ -6,17 +6,17 @@ require("class/auth/index.php");
 require("class/auth/sipx/settings.php");
 
 class HomerAuthentication extends Authentication {
-	                        
+                            
     function login($username, $password) {
       $m = new Mongo(SIPX_DB_URL, array("replicaSet" => "sipxecs"));
       $db = $m->imdb;
       $u = $db->entity->findOne(array("uid" => $username, "pntk" => $password));
       if ($u) {
-	if (in_array(SIPX_ADMIN_PERM, $u['prm'])) {
-          $_SESSION['loggedin'] = $username;
-          $_SESSION['userlevel'] = 1;
-	}
-	return true;
+          if (in_array(SIPX_ADMIN_PERM, $u['prm'])) {
+              $_SESSION['loggedin'] = $username;
+              $_SESSION['userlevel'] = 1;
+          }
+          return true;
       }
 
       return true;
