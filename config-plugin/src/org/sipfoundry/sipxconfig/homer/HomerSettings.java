d773c09f83a436459141268545fa21f13618350b
Douglas Hubler <dhubler@ezuce.com>
2012-08-13 17:17:04 -0400

    set timezone in homer to avoid err

--- config-plugin/src/org/sipfoundry/sipxconfig/homer/HomerSettings.java
+++ config-plugin/src/org/sipfoundry/sipxconfig/homer/HomerSettings.java
@@ -8,14 +8,28 @@
 package org.sipfoundry.sipxconfig.homer;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 
 import org.sipfoundry.sipxconfig.cfgmgt.DeployConfigOnEdit;
 import org.sipfoundry.sipxconfig.feature.Feature;
 import org.sipfoundry.sipxconfig.setting.PersistableSettings;
 import org.sipfoundry.sipxconfig.setting.Setting;
+import org.sipfoundry.sipxconfig.setting.SettingEntry;
 
 public class HomerSettings extends PersistableSettings implements DeployConfigOnEdit {
+    
+    public HomerSettings() {
+        addDefaultBeanSettingHandler(new Defaults());
+    }
+    
+    public static class Defaults {
+        @SettingEntry(path = "homer_web/timezone")
+        public String getTimeZone() {
+            // Cannot use TimeZoneManager because doesn't expose the ID
+            return Calendar.getInstance().getTimeZone().getID();            
+        }
+    }
 
     @Override
     public String getBeanId() {
