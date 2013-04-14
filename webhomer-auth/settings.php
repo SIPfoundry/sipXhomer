4ae3ae532039e0406add78ad5c16acdca4c40243
dizzy <george@ezuce.com>
2013-04-15 02:11:14 +0300

    PT47807803: Homer login fails if local mongo instance is in SECONDARY state
    - extract replicaset nodes from mongo-client.ini, use it in authentication

--- webhomer-auth/settings.php
+++ webhomer-auth/settings.php
@@ -1,6 +1,10 @@
 <?php
 
-define('SIPX_DB_URL',"mongodb://localhost");
+$ini_content = file("/etc/sipxpbx/mongo-client.ini");
+$line = $ini_content[0];
+$value = substr($line,strpos($line,'=') + 1, strlen($line));
+$rs = substr($value,0,strpos($value,'?'));
+define('SIPX_DB_URL', $rs);
 define('SIPX_ADMIN_PERM',"superadmin");
 
 ?>
